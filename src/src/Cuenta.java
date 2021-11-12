35e7f3758ed8fe289af0257bcfb965fb9537cc77
David <David@DESKTOP-SDL8PNV>
2021-11-12 10:05:20 +0100

    Commit version 1

---
+++ src/src/Cuenta.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package src;
+
+/**
+ *
+ * @author David
+ */
+public class Cuenta {
+    
+    private int saldo;
+    
+    Cuenta(int saldo){
+        this.saldo = saldo;
+    }
+    
+    public int Reintegro(){
+        return 0;
+    }
+    
+    public int deposito(int ingreso){
+        return 0;
+    }
+}
